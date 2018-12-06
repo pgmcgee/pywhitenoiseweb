fc3cb846477660ea85f745a33e6da8168c09d283
Parker McGee <parker@thosemcgees.com>
2018-12-06 17:41:15 -0500

    Refactor a bit to better parameterize

--- pywhitenoiseweb.py
+++ pywhitenoiseweb.py
@@ -5,10 +5,15 @@ import random
 import os
 import time
 
+page_max = 6
+viewsleep_max = 60
+loopsleep_max = 60
+breaktime_max = 1200
+
 while True:
     session = HTMLSession()
 
-    page = random.randrange(1,6)
+    page = random.randrange(1, page_max)
     siteinfo_re = re.compile("\/siteinfo\/(.+)")
 
     site = "http://www.alexa.com/topsites/category;" + str(page) +"/Top/Business/"
@@ -25,22 +30,22 @@ while True:
     print("[*] Opening %s in Firefox" % link)
     webbrowser.get('firefox').open(link)
 
-    viewsleep = random.randrange(30,60)
+    viewsleep = random.randrange(viewsleep_max / 2, viewsleep_max)
     print("[*] Sleeping for %s seconds" % viewsleep)
     time.sleep(viewsleep)
 
+    print("[*] Killing Firefox")
     if os.name == 'nt':
         os.system("taskkill /f /im Firefox.exe")
     else:
-        print("[*] Killing Firefox")
         os.system("pkill firefox")
 
-    loopsleep = random.randrange(30, 60)
+    loopsleep = random.randrange(loopsleep_max / 2, loopsleep_max)
     print("[*] Sleeping for %s seconds" % loopsleep)
     time.sleep(loopsleep)
 
-    longbreak = random.randrange(0,4)
+    longbreak = random.randrange(0, 4)
     if longbreak == 0:
-        breaktime = random.randrange(240, 1200)
+        breaktime = random.randrange(breaktime_max / 5, breaktime_max)
         print("[*] Taking a long break of %s seconds..." % breaktime)
         time.sleep(breaktime)
